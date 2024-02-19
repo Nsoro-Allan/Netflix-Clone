b8dcc964a25c0afa940886c9e6c3713f51e8cc72
AllanCorp <135638821+allancorp23@users.noreply.github.com>
2024-02-19 17:47:46 +0200

    init

---
+++ main.js
@@ -0,0 +1,56 @@
+// function one starts here
+function show1(){
+    document.getElementById("faq1").style.display="flex";
+}
+function hide1(){
+    document.getElementById("faq1").style.display="none";
+}
+
+// function one ends here
+
+// function two starts here
+function show2(){
+    document.getElementById("faq2").style.display="flex";
+}
+function hide2(){
+    document.getElementById("faq2").style.display="none";
+}
+
+// function two ends here
+
+// function three starts here
+function show3(){
+    document.getElementById("faq3").style.display="flex";
+}
+function hide3(){
+    document.getElementById("faq3").style.display="none";
+}
+// function three ends here
+
+// function four starts here
+function show4(){
+    document.getElementById("faq4").style.display="flex";
+}
+function hide4(){
+    document.getElementById("faq4").style.display="none";
+}
+// function four ends here
+
+// function five starts here
+function show5(){
+    document.getElementById("faq5").style.display="flex";
+}
+function hide5(){
+    document.getElementById("faq5").style.display="none";
+}
+// function five ends here
+
+// function six starts here
+function show6(){
+    document.getElementById("faq6").style.display="flex";
+}
+function hide6(){
+    document.getElementById("faq6").style.display="none";
+}
+// function six ends here
+
